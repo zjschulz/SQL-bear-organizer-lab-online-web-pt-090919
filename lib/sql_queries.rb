840b184f373b564fd5cdf03cb488d121730a602e
Zachary Schulz <zjschulz@gmail.com>
2019-11-10 12:50:04 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT max(color), COUNT(*) FROM bears"
+  "SELECT max(color), COUNT(*) FROM bears GROUP BY color"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
